433117a252984b16485d02ccfca6bc994f5f762f
Kevin Ou <therealkevinou@yahoo.com>
2019-05-05 15:03:32 -0500

    updated upload component to use material ui

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,19 +1,56 @@
 import React from 'react';
 import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 
-const handleClick = async () => {
-  const fileChooser = document.getElementById('file-chooser');
-  const file = fileChooser.files[0];
-  const { name, type } = file;
-  const { data } = await axios.post('/upload', { name, type });
-  const uploadResponse = await axios.put(data, file);
-  console.log(uploadResponse)
-};
+class Upload extends React.Component {
+  constructor() {
+    super();
 
-export default () => (
-  <>
-    <input type="file" id="file-chooser" />
-    <button id="upload-button" onClick={handleClick}>Upload to S3</button>
-    <div id="results"></div>
-  </>
-);
+    this.state = {
+      fileList: [],
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      fileList: target.files,
+    });
+  }
+
+  async handleClick() {
+    const { fileList } = this.state;
+    const file = fileList[0];
+    const { name, type } = file;
+    const { data } = await axios.post('/upload', { name, type });
+    const uploadResponse = await axios.put(data, file);
+  }
+
+  render() {
+    const { fileList } = this.state;
+    const containsNoFile = fileList.length === 0;
+
+    return (
+      <div className="upload">
+        <Input
+          onChange={this.handleChange}
+          type="file"
+          id="file-chooser"
+          disableUnderline
+        />
+        <Button
+          color="primary"
+          disabled={containsNoFile}
+          onClick={this.handleClick}
+          variant="contained"
+        >
+        Upload Picture
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default Upload;
