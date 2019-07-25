d484cdaf0ff5146352c1f912392167b8d0864a75
Kevin Ou <therealkevinou@yahoo.com>
2019-07-24 22:47:35 -0500

    removing service worker, added in production entry point

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
 const upload = require('./upload');
 
+const BUILD_PATH = '../../build';
 const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false, parameterLimit: 1000000 }));
 
+app.use(express.static(path.join(__dirname, BUILD_PATH)));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, BUILD_PATH, 'index.html'));
+});
 app.post('/upload', upload);
 
 // eslint-disable-next-line no-console
